bddd1a7d0376b938f059cb7856dac09dab279806
MikeCheng1208 <a3804430@hotmail.com>
2018-05-25 16:10:53 +0800

    add mainnet url

--- app/scripts/lib/ipfsContent.js
+++ app/scripts/lib/ipfsContent.js
@@ -15,6 +15,7 @@ module.exports = function (provider) {
             resolver.resolve(name, provider).then(ipfsHash => {
                 clearTimeout(clearTime)
                 let url = 'https://gateway.ipfs.io/ipfs/' + ipfsHash
+                console.log('url=>',url);
                 return fetch(url, { method: 'HEAD' }).then(response => response.status).then(statusCode => {
                     if (statusCode !== 200) return 'Local'
                     extension.tabs.update(tab.id, { url: url })
